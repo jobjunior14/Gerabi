03ec5ef8ff6a88fbefd5359e105b3e9089e9bf36
Job Junior <102473673+jobjunior14@users.noreply.github.com>
2024-01-05 23:32:25 +0200

    protected routes

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -51,6 +51,6 @@ userSchema.pre ('save', async function (next){
     next();
 
 });
-const User = mongoose.model ( 'User', userSchema);
+const User = mongoose.model ( 'user', userSchema);
 
 module.exports = User;
